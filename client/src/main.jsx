b9a4824e2ad75600b9e65a96007a026e562d742a
AlgorythmRK <royalraghu53@gmai.com>
2025-12-06 18:54:56 +0530

    Prototype - Not Deployed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,11 +9,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <ToastProvider>
-          <AuthProvider>
+      <ToastProvider>
+        <AuthProvider>
           <App />
-          </AuthProvider>
-        </ToastProvider>
+        </AuthProvider>
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
